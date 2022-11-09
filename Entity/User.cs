c6fe331002349046e8ba2e835a9b76c9cb69be80
gorkemtepe <gorkemtepee@hotmail.com>
2022-11-09 20:23:08 +0300

    Add project files.

---
+++ Entity/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Entity
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [StringLength(50)]
+        public string? FullName { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Password { get; set; }
+        public bool Locked { get; set; }=false;
+        public DateTime CreateAt { get; set; } = DateTime.Now;
+    }
+
+}
